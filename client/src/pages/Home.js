1999abef822ec28d3e6294ad22dee318b4d30172
Austin Thorpe <Austin96.at@gmail.com>
2020-12-18 12:46:24 -0600

    books

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -69,7 +69,7 @@ class Home extends Component {
                             value={this.state.search}
                             onChange={this.handleInputChange}
                             name="search"
-                            placeholder="e.g. Harry Potter"
+                            placeholder="Unfortunate Events"
                         />
                         <SubmitBtn onClick={this.handleFormSubmit} />
                     </form>
