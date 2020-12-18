1e49faee5a65cf1d6aebb64d9fee7be3301537b6
Austin Thorpe <Austin96.at@gmail.com>
2020-12-18 00:39:41 -0600

    books

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4 text-center inputText">Google Books Search</h1>
+                <br></br>
+                <h2 className="text-center inputText">Search for fantastic books and you can also save some of your favorites!</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
